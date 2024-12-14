0972478905d484e2e46504c879a779e1bd4723be
Antwhale <olollolo339@naver.com>
2024-12-14 19:29:20 +0900

    Initial Commit

---
+++ GreenGroceryCloneApp/GreenGroceryCloneAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GreenGroceryCloneAppApp.swift
+//  GreenGroceryCloneApp
+//
+//  Created by 부재식 on 12/14/24.
+//
+
+import SwiftUI
+
+@main
+struct GreenGroceryCloneAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
